2b564ca03fe6fce9c74e7a9c4ead20e2777e3d96
Alex T <tishchenko@yahoo.com>
2025-03-24 16:17:55 -0500

    S3 ACL and encryption

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "15gth-aldev-bucket"
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_server_side_encryption" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
